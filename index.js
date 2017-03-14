d1194727af9e603a518451c6fe101163052958d6
Gururaj Sridhar <gururaj.sridhar@autodesk.com>
2017-03-14 13:17:56 -0400

    fixing routes

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ var app = express();
 require('./config.js')(app);
 
 var guides = require('./models/guidesSchema');
-
-require('./routes/routes.js')(app, guides);
+require('./models/mongoose.js');
+var routes = require('./routes/routes.js')(app, guides);
+app.use('/', routes);
 
 app.listen(app.get('port'), function() {
     console.log('Server running:' + app.get('port'));
